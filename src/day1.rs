8fb1d4589fb69395a8c73db4267409037416df2b
Ryan Davis <ryepup@gmail.com>
2024-12-02 01:38:25 +0000

    cleanup a bit
    - de-dupe the parsing
    - return errors instead of panicking
    - use more iterators
    - use more generics to resolve some wacky stuff with `String` vs `str`
    
    Couldn't get the `parse` function to return an iterator, had to pull everything into memory. I think there's a way there with more exotic types like `impl Iterator` but I'm done messing with it for the night.

--- src/day1.rs
+++ src/day1.rs
@@ -1,93 +1,148 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Error};
+use std::num::ParseIntError;
 use std::path::Path;
+use std::{error, fmt};
 
-pub fn solve1<P>(filename: P) -> i32
+pub fn solve1<P>(filename: P) -> Result<i32, ParseError>
 where
     P: AsRef<Path>,
 {
-    let mut vecA: Vec<i32> = Vec::new();
-    let mut vecB: Vec<i32> = Vec::new();
+    let mut left_list: Vec<i32> = Vec::new();
+    let mut right_list: Vec<i32> = Vec::new();
 
-    if let Ok(lines) = read_lines(filename) {
-        for line in lines.flatten() {
-            let array: Vec<i32> = line
-                .split("   ")
-                .map(|x| x.parse::<i32>().unwrap())
-                .collect();
+    for (a, b) in parse(filename)? {
+        let pos = left_list.binary_search(&a).unwrap_or_else(|e| e);
+        left_list.insert(pos, a);
 
-            assert_eq!(array.len(), 2, "should have the same len");
-
-            let pos = vecA.binary_search(&array[0]).unwrap_or_else(|e| e);
-            vecA.insert(pos, array[0]);
-
-            let pos = vecB.binary_search(&array[1]).unwrap_or_else(|e| e);
-            vecB.insert(pos, array[1]);
-        }
-
-        let mut total: i32 = 0;
-        for (i, a) in vecA.iter().enumerate() {
-            let b = vecB[i];
-
-            total += (b - a).abs();
-        }
-
-        return total;
+        let pos = right_list.binary_search(&b).unwrap_or_else(|e| e);
+        right_list.insert(pos, b)
     }
-    panic!("how do I return errors?")
+
+    let total = left_list
+        .iter()
+        .zip(right_list)
+        .fold(0, |acc, (a, b)| acc + (b - a).abs());
+    Ok(total)
 }
 
-pub fn solve2<P>(filename: P) -> i32
+pub fn solve2<P>(filename: P) -> Result<i32, ParseError>
 where
     P: AsRef<Path>,
 {
-    let mut vecA: Vec<i32> = Vec::new();
-    let mut freqB: HashMap<i32, i32> = HashMap::new();
+    let mut left_list: Vec<i32> = Vec::new();
+    let mut right_histogram: HashMap<i32, i32> = HashMap::new();
 
-    if let Ok(lines) = read_lines(filename) {
-        for line in lines.flatten() {
-            let array: Vec<i32> = line
-                .split("   ")
-                .map(|x| x.parse::<i32>().unwrap())
-                .collect();
+    for (a, b) in parse(filename)? {
+        let pos = left_list.binary_search(&a).unwrap_or_else(|e| e);
+        left_list.insert(pos, a);
 
-            assert_eq!(array.len(), 2, "should have the same len");
-
-            let pos = vecA.binary_search(&array[0]).unwrap_or_else(|e| e);
-            vecA.insert(pos, array[0]);
-
-            match freqB.get(&array[1]) {
-                Some(&count) => freqB.insert(array[1], count + 1),
-                _ => freqB.insert(array[1], 1),
-            };
-        }
-
-        let mut total: i32 = 0;
-        for a in vecA.iter() {
-            match freqB.get(a) {
-                Some(&count) => total += a * count,
-                None => { /* nothing to do */ }
-            };
-        }
-
-        return total;
+        right_histogram.insert(b, 1 + right_histogram.get(&b).copied().unwrap_or_default());
     }
-    panic!("how do I return errors?")
+
+    let total = left_list
+        .iter()
+        .map(|x| x * right_histogram.get(x).copied().unwrap_or_default())
+        .sum();
+    Ok(total)
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+/// custom error wrapping everything that might fail.
+///
+/// feels like a more verbose version of java checked exceptions...
+#[derive(Debug)]
+pub enum ParseError {
+    Read(Error),
+    Parse(ParseIntError),
+}
+
+// print prettily
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ParseError::Read(..) => write!(f, "data file could not be read"),
+            ParseError::Parse(..) => write!(f, "the provided string could not be parsed as int"),
+        }
+    }
+}
+
+// magic to unwrap the error if needed
+impl error::Error for ParseError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            ParseError::Read(ref e) => Some(e),
+            ParseError::Parse(ref e) => Some(e),
+        }
+    }
+}
+
+// convert from our base errors into out custom error
+impl From<ParseIntError> for ParseError {
+    fn from(err: ParseIntError) -> ParseError {
+        ParseError::Parse(err)
+    }
+}
+
+impl From<Error> for ParseError {
+    fn from(err: Error) -> ParseError {
+        ParseError::Read(err)
+    }
+}
+
+/// parse the file into tuples representing each list
+fn parse<P>(filename: P) -> Result<Vec<(i32, i32)>, ParseError>
 where
     P: AsRef<Path>,
 {
+    match read_lines(filename) {
+        Ok(lines) => match lines.flatten().map(parse_line).collect() {
+            Ok(v) => Ok(v),
+            Err(e) => Err(e.into()),
+        },
+        Err(e) => Err(e.into()),
+    }
+}
+
+/// parse a line into a tuple of integers
+///
+/// this `S: AsRef<str>` jazz allows calling this function with `str` _or_
+/// `String`. Rust seems to use generics as a mechanism for overloading.
+fn parse_line<S: AsRef<str>>(line: S) -> Result<(i32, i32), ParseIntError> {
+    let parts: Result<Vec<_>, _> = line
+        .as_ref()
+        .split("   ")
+        .map(|s| s.parse::<i32>())
+        .collect();
+    match parts {
+        Ok(v) => {
+            assert_eq!(v.len(), 2);
+            Ok((v[0], v[1]))
+        }
+        Err(e) => Err(e),
+    }
+}
+
+// return an iterator over all the lines in the file
+fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn test_testing() {
-        assert_eq!(1, 1);
+    fn test_split() {
+        assert_eq!(
+            vec!["1234", "5678"],
+            "1234   5678".split("   ").collect::<Vec<_>>()
+        );
+    }
+
+    #[test]
+    fn test_parse_line() {
+        assert_eq!(Ok((1234, 5678)), parse_line("1234   5678"))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod day1;
 
 fn main() {
-    println!("Part 1: {}", day1::solve1("./data/day1.txt"));
-    println!("Part 1: {}", day1::solve2("./data/day1.txt"));
+    println!("Part 1: {}", day1::solve1("./data/day1.txt").unwrap());
+    println!("Part 2: {}", day1::solve2("./data/day1.txt").unwrap());
 }
