3f3d03914ddd95465be04b32b626bcf05694ff1c
Ryan Davis <ryepup@gmail.com>
2024-12-03 02:40:49 +0000

    day 2 pt 1
    main.rs has some magic abilities to declare modules from other files by virtue of the `mod` keyword.
    
    Error handling still feels wrong. The `and_then` and `map_err` feel closer to the right solution, but I'm still missing a lot. Making my own error without wrapping is a lot less verbose, but it's super lossy.

---
+++ src/day2.rs
@@ -0,0 +1,98 @@
+use std::{fmt, path::Path};
+
+use crate::util;
+// Define our error types. These may be customized for our error handling cases.
+// Now we will be able to write our own errors, defer to an underlying error
+// implementation, or do something in between.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Day2Error;
+
+impl fmt::Display for Day2Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "something went wrong!")
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Day2Error>;
+
+pub fn solve1<P>(filename: P) -> Result<usize>
+where
+    P: AsRef<Path>,
+{
+    parse(filename).and_then(|reports| Ok(reports.iter().filter(|r| r.safe()).count()))
+}
+
+pub fn solve2<P>(filename: P) -> Result<i32>
+where
+    P: AsRef<Path>,
+{
+    Ok(0)
+}
+
+fn parse<P: AsRef<Path>>(filename: P) -> Result<Vec<Report>> {
+    util::read_lines(filename)
+        .map_err(|_| Day2Error)?
+        .flatten()
+        .map(parse_line)
+        .collect()
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Report {
+    levels: Vec<i32>,
+}
+
+impl Report {
+    fn safe(&self) -> bool {
+        let mut prev = self.levels[0];
+        let mut is_increasing: Option<bool> = None;
+        for curr in self.levels.iter().skip(1) {
+            match (curr - prev, is_increasing) {
+                (0, _) => return false,
+                (d, _) if d.abs() > 3 => return false,
+                (d, Some(true)) if d < 0 => return false,
+                (d, Some(false)) if d > 0 => return false,
+                (d, None) => is_increasing = Some(d > 0),
+                (_, _) => (), // OK
+            };
+
+            prev = *curr
+        }
+        true
+    }
+}
+
+fn parse_line<S: AsRef<str>>(line: S) -> Result<Report> {
+    let levels: std::result::Result<Vec<_>, _> =
+        line.as_ref().split(" ").map(|s| s.parse::<i32>()).collect();
+    match levels {
+        Ok(l) => Ok(Report { levels: l }),
+        Err(_) => Err(Day2Error),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_line() {
+        assert_eq!(
+            Ok(Report {
+                levels: vec![1, 2, 7, 8, 9]
+            }),
+            parse_line("1 2 7 8 9")
+        )
+    }
+
+    #[test]
+    fn test_safe() -> Result<()> {
+        assert_eq!(true, parse_line("7 6 4 2 1")?.safe());
+        assert_eq!(false, parse_line("1 2 7 8 9")?.safe());
+        assert_eq!(false, parse_line("9 7 6 2 1")?.safe());
+        assert_eq!(false, parse_line("1 3 2 4 5")?.safe());
+        assert_eq!(false, parse_line("8 6 4 4 1")?.safe());
+        assert_eq!(true, parse_line("1 3 6 7 9")?.safe());
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-mod day1;
+mod day2;
+mod util;
 
 fn main() {
-    println!("Part 1: {}", day1::solve1("./data/day1.txt").unwrap());
-    println!("Part 2: {}", day1::solve2("./data/day1.txt").unwrap());
+    println!("Part 1: {}", day2::solve1("./data/day2.txt").unwrap());
+    println!("Part 2: {}", day2::solve2("./data/day2.txt").unwrap());
 }
